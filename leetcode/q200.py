de2bc9c14179896ab7cf14817e9857b5cd4ca413
CrysM28 <chrm0208@gmail.com>
2022-07-22 20:22:18 +0900

    Problem Solving 220722

---
+++ leetcode/q200.py
@@ -0,0 +1,14 @@
+# Number of Islands
+## yet solving
+
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        pass
+
+
+# testcase
+g = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+Solution().numIslands(g)
+
--- python-for-coding-test/ch4-q2.py
+++ python-for-coding-test/ch4-q2.py
@@ -1,21 +1,20 @@
 # 왕실의 나이트
-## 푸는중
+## 풀이 참고
 
-# 바둑판
-x = [x for x in range(1, 9)]
-y = ["a","b","c","d","e","f","g","h"]
-
-# 나이트 위치
+# 나이트 현재 위치
 a = input()
-nx = a[1:]
-ny = a[:1]
+row = int(a[1])
+col = int(ord(a[0]) - ord('a')) + 1
 
-step = []   # 가능한 움직임
-move = 0    
+# 나이트가 이동할 수 있는 방향 정의
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
 
-for i in range(1,9):
-
-    pass
-
-
+# 이동가능한지 확인
+move = 0
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+    if 1 <= next_row <= 8 and 1<= next_col <= 8:
+        move += 1
+        
 print(move)
