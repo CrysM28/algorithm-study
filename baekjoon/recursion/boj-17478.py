017c0244efc1fd588b8c4a75985f819830bbca09
CrysM28 <chrm0208@gmail.com>
2022-08-06 19:47:42 +0900

    Solving boj-step9-recursion

--- baekjoon/recursion/boj-17478.py
+++ baekjoon/recursion/boj-17478.py
@@ -1,21 +1,24 @@
-def chatbot(n):  
-    if n <1:
-        chatbot(n+1)
-    print('"재귀함수가 뭔가요?"')
-    print('"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
-    print('마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
-    print('그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
-    print('"재귀함수가 뭔가요?"')
+# 재귀함수가 뭔가요?
 
-    # if n>1:
-    #     chatbot(n-1)
-    # elif n == 1:
-    #     print('"재귀함수는 자기 자신을 호출하는 함수라네"')
+def chatbot(n, max_n):
+    prefix = '_' * (max_n - n) * 4
+    if n == 0:
+        print('{}"재귀함수가 뭔가요?"'.format(prefix))
+        print('{}"재귀함수는 자기 자신을 호출하는 함수라네"'.format(prefix))
+        print('{}라고 답변하였지.'.format(prefix))
+        return
 
-    # print('라고 답변하였지.')
+    print('{}"재귀함수가 뭔가요?"'.format(prefix))
+    print('{}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'.format(prefix))
+    print('{}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'.format(prefix))
+    print('{}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'.format(prefix))
+
+    chatbot(n - 1, max_n)
+
+    print('{}라고 답변하였지.'.format(prefix))
 
 
 num = int(input())
 
 print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
-chatbot(num)
+chatbot(num, num)
---
+++ baekjoon/recursion/boj-2447.py
@@ -0,0 +1,11 @@
+# 별 찍기
+
+def star(n):
+    print("*"*3)
+    print("* *")
+    print("*"*3)
+
+num = int(input())
+
+
+star(num)
